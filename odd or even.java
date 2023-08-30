d2d57284dd5677c3177f54d13d928b0d86b7db95
vasumathi475 <142963096+vasumathi475@users.noreply.github.com>
2023-08-30 18:36:20 +0530

    Completed

---
+++ odd or even.java
@@ -0,0 +1,9 @@
+class oddoreven {
+ public static void main(String args[]){
+   int num = 3;
+   if (num % 2 ==0)
+     System.out.println("The given number is ODD");
+   else 
+     System.out.println("The given number is EVEN");
+ }
+}
