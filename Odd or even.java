58039475125a6df7c4f051a9663131de7e9b09a0
vasumathi475 <142963096+vasumathi475@users.noreply.github.com>
2023-08-30 18:10:59 +0530

    Create Odd or even.java
    Odd or even.java

---
+++ Odd or even.java
@@ -0,0 +1,9 @@
+class odd or even {
+  public static void main (String args[]){
+    int num = 3;
+    if( num % 2 == 0)
+      System.out.println("The given number is ODD");
+    else
+      System.out.println("The given number is EVEN");
+  }
+}
